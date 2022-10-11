c57d8f19315ea0ca59bb74a613216e8d01cab9ed
splinter <you@example.com>
2022-10-11 16:25:10 +0800

    06 useState Hook

--- src/Content.js
+++ src/Content.js
@@ -1,32 +1,32 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Content = () => {
+  const [name, setName] = useState('Dan')
+  const [count, setCount] = useState(0)
+
   const handleNameChange = () => {
     const names = ['Dan', 'Jenny', 'Lloyd']
     const int = Math.floor(Math.random() * 3);
-    return names[int];
+    setName(names[int]);
   }
 
   const handleClick = () => {
-    console.log('You clicked it')
+    setCount(count + 1)
+    setCount(count + 1)
+    console.log(count)
   }
 
   const handleClick2 = (name) => {
-    console.log(`${name} was clicked`)
-  }
-
-  const handleClick3 = (e) => {
-    console.log(e);
-    console.log(e.target);
-    console.log(e.target.innerText);
+    console.log(count)
   }
 
   return (
     <main>
-      <p onDoubleClick={handleClick}>Welcome {handleNameChange()}, to your Dashboard</p>
+      <p onDoubleClick={handleClick}>Hello {name}!</p>
+
+      <button onClick={handleNameChange}>Change Name</button>
       <button onClick={handleClick}>Click It</button>
-      <button onClick={() => handleClick2('Dan')}>Click It</button>
-      <button onClick={(e) => handleClick3(e)}>Click It</button>
+      <button onClick={handleClick2}>Click It</button>
     </main>
   )
 }
