e85ffc9ce715de2d5434f793dc15bfa913cc5d11
splinter <you@example.com>
2022-10-11 14:47:58 +0800

    02 App & JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleNameChange = () => {
+    const names = ['Dan', 'Jenny', 'Lloyd']
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Home Page</h1>
+      <p>Welcome <b>{handleNameChange()}</b>, to your personal Dashboard</p>
     </div>
   );
 }
